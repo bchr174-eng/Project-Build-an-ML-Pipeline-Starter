09d371dd833573373286e1ec5963c6fb7b621ccd
bob <bchr174@wgu.edu>
2026-03-11 14:16:16 -0600

    working on basic_cleaning

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def go(config: DictConfig):
                 parameters={
                     "input_artifact": "sample.csv:latest",
                     "output_artifact": "clean_sample.csv",
-                    "output_type:": "clean_sample",
+                    "output_type": "clean_sample",
                     "output_description": "Takes input and cleans the data",
                     "min_price": config["etl"]["min_price"],
                     "max_price": config["etl"]["max_price"]
